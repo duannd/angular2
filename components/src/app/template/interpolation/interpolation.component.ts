fd7529081554a601a27e5b34f21a353c9b44c637
duan.nguyen <duan.nguyen@formos.com>
2019-07-05 18:29:00 +0700

    Add Interpolation section

--- components/src/app/template/interpolation/interpolation.component.ts
+++ components/src/app/template/interpolation/interpolation.component.ts
@@ -7,10 +7,22 @@ import {Component, OnInit} from '@angular/core';
 })
 export class InterpolationComponent implements OnInit {
 
+  currentCustomer: string;
+  count: number;
+
   constructor() {
   }
 
   ngOnInit() {
+    this.count = 0;
+    this.currentCustomer = 'Danny';
+    setInterval(() => {
+      this.count++;
+    }, 1000);
+  }
+
+  getVal() {
+    return 3;
   }
 
 }
