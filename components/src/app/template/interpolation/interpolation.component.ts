5c2e39b1488adcd03b0b77363d58ebc3874a3839
Duan Nguyen Dinh <ngdduan@gmail.com>
2019-07-06 09:26:33 +0700

    Components & Templates :: Template syntax :: finish interpolation section

--- components/src/app/template/interpolation/interpolation.component.ts
+++ components/src/app/template/interpolation/interpolation.component.ts
@@ -9,6 +9,8 @@ export class InterpolationComponent implements OnInit {
 
   currentCustomer: string;
   count: number;
+  imageUrl: string;
+  imageAlt = 'Angular Avatar';
 
   constructor() {
   }
@@ -16,6 +18,7 @@ export class InterpolationComponent implements OnInit {
   ngOnInit() {
     this.count = 0;
     this.currentCustomer = 'Danny';
+    this.imageUrl = '';
     setInterval(() => {
       this.count++;
     }, 1000);
