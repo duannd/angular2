5c8fafa9abdf6a71ca818fc7ccf89e0bee55f23c
duan.nguyen <duan.nguyen@formos.com>
2019-07-23 13:17:02 +0700

    Commit Pass data from parent to child with input binding section

--- components/src/app/component-interaction/component-interaction.module.ts
+++ components/src/app/component-interaction/component-interaction.module.ts
@@ -4,10 +4,11 @@ import {CommonModule} from '@angular/common';
 import {ComponentInteractionRoutingModule} from './component-interaction-routing.module';
 import { InteractionComponent } from './interaction.component';
 import { HeroParentComponent } from './hero-parent.component';
+import { HeroChildComponent } from './hero-child.component';
 
 
 @NgModule({
-  declarations: [InteractionComponent, HeroParentComponent],
+  declarations: [InteractionComponent, HeroParentComponent, HeroChildComponent],
   imports: [
     CommonModule,
     ComponentInteractionRoutingModule
--- components/src/app/component-interaction/hero-parent.component.ts
+++ components/src/app/component-interaction/hero-parent.component.ts
@@ -1,19 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import {Hero, HEROES} from '../hero';
 
 @Component({
   selector: 'app-hero-parent',
   template: `
-    <p>
-      hero-parent works!
-    </p>
+    <h3>{{masterName}} controls {{heroes.length}} heroes</h3>
+    <app-hero-child *ngFor="let hero of heroes" [hero]="hero" [master]="masterName"></app-hero-child>
   `,
   styles: []
 })
 export class HeroParentComponent implements OnInit {
 
+  masterName: string;
+  heroes: Hero[];
+
   constructor() { }
 
   ngOnInit() {
+    this.masterName = 'Duan Nguyen';
+    this.heroes = HEROES;
   }
 
 }
