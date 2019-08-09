7afb49d9bf29a6dd4c401074de8f676a64fdcf57
duan.nguyen <duan.nguyen@formos.com>
2019-08-09 13:53:57 +0700

    Observables & RxJS :: complete observable basic

--- observables-rxjs/src/app/basic/basic.component.ts
+++ observables-rxjs/src/app/basic/basic.component.ts
@@ -1,66 +1,112 @@
-import {Component, OnInit} from '@angular/core';
-import {Observable} from 'rxjs';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
+import {Observable, of, Subscription} from 'rxjs';
+import {delay} from 'rxjs/operators';
 
-// Create an Observable that will start listening to geolocation updates when a consumer subscribes.
-console.log('Create an Observable that will start listening to geolocation updates when a consumer subscribes.');
-const locations = new Observable<any>((observe) => {
-  // Get the next and error callbacks. These will be passed in when the consumer subscribes.
-  console.log('Get the next and error callbacks. These will be passed in when the consumer subscribes.');
-  const {next, error} = observe;
-  let watchId;
-  // Simple geolocation API check provides values to publish
-  console.log('Simple geolocation API check provides values to publish');
-  if ('geolocation' in navigator) {
-    watchId = navigator.geolocation.watchPosition(next, error);
-  } else {
-    error('Geolocation not available');
-  }
-  // When the consumer unsubscribe, clean up data ready for next subscription.
-  console.log('When the consumer unsubscribe, clean up data ready for next subscription.');
-  return {
-    unsubscribe(): void {
-      console.log('locations unsubscribe...');
-      navigator.geolocation.clearWatch(watchId);
-    }
-  };
-});
-
-// Call subscribe() to start listening for updates.
-const locationsSubscription = locations.subscribe({
-  next(position) {
-    console.log('Current Position: ', position);
+const ESC_KEY = 27;
+// Define myObserver
+const myObserver = {
+  next: (value) => {
+    console.log(`My Observer see next function :: value: ${value}`);
   },
-  error(msg) {
-    console.log('Error Getting Location: ', msg);
+  error: (error) => {
+    console.error(`My Observer see error handler: ${error}`);
+  },
+  complete: () => {
+    console.log('My Observer see complete handler');
   }
-});
+};
+// Define/Create simple observable that emits three values
+const myObservable = of(1, 2, 3).pipe(delay(400));
+
+// Create observable with constructor
+function sequenceSubscribe(observer) {
+  // synchronously deliver 1, 2, and 3, then complete
+  observer.next(1);
+  observer.next(2);
+  observer.next(4);
+  observer.complete();
+  // unsubscribe function doesn't need to do anything in this
+  // because values are delivered synchronously
+  const subscription = new Subscription(() => {
+    console.log('Sequence Subscription unsubscribe');
+  });
+  // return a subscription
+  console.log('Sequence Subscribe initial success then return a subscription');
+  return subscription;
+}
+
+// Create a new Sequence Observable that will deliver the above sequence
+const sequence = new Observable(sequenceSubscribe).pipe(delay(1000));
+
+// Create a Observable from Event.
+function fromEvent(target, eventName) {
+  return new Observable((observer) => {
+    const handler = (e) => observer.next(e);
+    // Add the event handler to the target
+    target.addEventListener(eventName, handler);
+    return () => {
+      // Detach the event handler from the target
+      target.removeEventListener(eventName, handler);
+    };
+  });
+}
 
 @Component({
   selector: 'app-basic',
   templateUrl: './basic.component.html',
   styles: []
 })
-export class BasicComponent implements OnInit {
+export class BasicComponent implements OnInit, AfterViewInit {
 
-  // locationsSubscription: Subscription;
+  mySubscription: Subscription;
+  sequenceSubscription: Subscription;
+  fromEventSubscription: Subscription;
 
   constructor() {
   }
 
   ngOnInit() {
-    console.log('BasicComponent onInit');
-    // this.locationsSubscription = locations.subscribe({
-    //   next(position) {
-    //     console.log(`Current position: ${position}`);
-    //   },
-    //   error(msg) {
-    //     console.log('Error Getting Location: ', msg);
-    //   }
-    // });
   }
 
-  unsubscribe() {
-    locationsSubscription.unsubscribe();
+  ngAfterViewInit(): void {
+    const nameInput = document.getElementById('name') as HTMLInputElement;
+    // register a subscribe fromEvent => OK, it is running.
+    this.fromEventSubscription = fromEvent(nameInput, 'keyup').subscribe(
+      (e: KeyboardEvent) => {
+        if (e.keyCode === ESC_KEY) {
+          nameInput.value = '';
+        }
+      }
+    );
+  }
+
+  subscribeMyObservable() {
+    // Execute with the observer object
+    this.mySubscription = myObservable.subscribe(myObserver);
+  }
+
+  unsubscribeMyObservable() {
+    if (this.mySubscription) {
+      console.log('Unsubscribe myObservable');
+      this.mySubscription.unsubscribe();
+      this.mySubscription = undefined;
+    }
+  }
+
+  subscribeSequence() {
+    this.sequenceSubscription = sequence.subscribe(
+      value => console.log(`Next value: ${value}`),
+      error => console.error(`Error: ${error}`),
+      () => console.log('The End subscribe Sequence')
+    );
+  }
+
+  unsubscribeSequence() {
+    if (this.sequenceSubscription) {
+      console.log('Unsubscribe SequenceObservable');
+      this.sequenceSubscription.unsubscribe();
+      this.sequenceSubscription = undefined;
+    }
   }
 
 }
