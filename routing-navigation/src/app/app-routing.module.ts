a741f41904fb0904f02fe284afa017d7e2a84295
duan.nguyen <duan.nguyen@formos.com>
2019-09-05 15:18:34 +0700

    Routing & Navigation :: Complete Milestone 4: Crisis center feature :: Add popup router-outlet

--- routing-navigation/src/app/app-routing.module.ts
+++ routing-navigation/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+import {ComposeMessageComponent} from './compose-message/compose-message.component';
 
 
 const routes: Routes = [
+  {path: 'compose', component: ComposeMessageComponent, outlet: 'popup'},
   {
     path: 'crisis-center',
     loadChildren: () => import('./crisis-center/crisis-center.module').then(mod => mod.CrisisCenterModule),
